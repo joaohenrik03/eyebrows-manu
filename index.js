2c77f9af46bf56b31e8aa07ee06c65a3b81b6986
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-07 22:53:25 -0300

    Update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function onScroll() {
+function showNavOnScroll() {
     if (scrollY > 0) {
         document.querySelector('#nav').classList.add('scroll')
     } else {
         document.querySelector('#nav').classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 500) {
+        document.querySelector('#backToTop').classList.add('show')
+    } else {
+        document.querySelector('#backToTop').classList.remove('show')
     }
 }
 
@@ -22,5 +30,12 @@ ScrollReveal({
 }).reveal(`
     #home, #home img, #home .stats,
     #services header, #services .card,
-    #about, #about header, #about .content
+    #about, #about header, #about .content,
+    #contact, #contact header, #contact .content, #contact a, #contact img,
+    footer, footer .socials
 `)
+
+window.addEventListener('scroll', () => {
+    showNavOnScroll(),
+    showBackToTopButtonOnScroll()
+})
